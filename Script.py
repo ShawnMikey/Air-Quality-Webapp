2d928bd5ae722b6e04a08883583af39ddbfea626
Shawn Michael <87108127+ShawnMikey@users.noreply.github.com>
2024-10-03 17:27:25 +0700

    Update Script.py

--- Script.py
+++ Script.py
@@ -92,7 +92,7 @@ with st.sidebar:
     st.image("soundwave.webp")
 
     # Menambahkan quotes
-    st.write("Autobots Inferior, Soundwave Superior - Soundwave")
+    st.write("'Autobots Inferior, Soundwave Superior' - Soundwave")
   
     # Mengambil start date dan end date
     start_date, end_date = st.date_input(
